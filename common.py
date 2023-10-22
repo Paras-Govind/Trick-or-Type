63db424108267d165de8d59c330c37443f343034
Paras-Govind <govindparas17@gmail.com>
2023-10-22 11:58:09 +0100

    Add menu page

---
+++ common.py
@@ -0,0 +1,17 @@
+import pygame
+
+
+display_width = 800
+display_height = 600
+ 
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+
+clock = pygame.time.Clock()
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
--- main.py
+++ main.py
@@ -1,41 +1,21 @@
 import pygame
 import time
-import random
+
+from common import *
+from menu import game_intro
  
 pygame.init()
  
-display_width = 800
-display_height = 600
- 
-black = (0,0,0)
-white = (255,255,255)
-red = (255,0,0)
- 
-block_color = (53,115,255)
- 
 ghost_width = 73
- 
-gameDisplay = pygame.display.set_mode((display_width,display_height))
+
 pygame.display.set_caption('Trick or Type')
-clock = pygame.time.Clock()
+
  
 ghostImg = pygame.image.load('assets/images/home_ghost.jpg')
  
- 
-def things_dodged(count):
-    font = pygame.font.SysFont(None, 25)
-    text = font.render("Dodged: "+str(count), True, black)
-    gameDisplay.blit(text,(0,0))
- 
-def things(thingx, thingy, thingw, thingh, color):
-    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
- 
 def ghost(x,y):
     gameDisplay.blit(ghostImg,(x,y))
- 
-def text_objects(text, font):
-    textSurface = font.render(text, True, black)
-    return textSurface, textSurface.get_rect()
+
  
 def message_display(text):
     largeText = pygame.font.Font('freesansbold.ttf',115)
@@ -48,31 +28,6 @@ def message_display(text):
     time.sleep(2)
  
     game_loop()
-    
-def crash():
-    message_display('You Crashed')
-
-def game_intro():
-
-    intro = True
-
-    while intro:
-        for event in pygame.event.get():
-            print(event)
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            elif event.type==pygame.KEYDOWN:
-                if event.key==pygame.K_RETURN:
-                    intro = False
-                
-        gameDisplay.fill(white)
-        largeText = pygame.font.Font('freesansbold.ttf',115)
-        TextSurf, TextRect = text_objects("Trick or Type", largeText)
-        TextRect.center = ((display_width/2),(display_height/2))
-        gameDisplay.blit(TextSurf, TextRect)
-        pygame.display.update()
-        clock.tick(15)
     
 def game_loop():
  
