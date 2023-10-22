ffed40dd6b94fa81ffe1d11aff1571750f5ad7d3
Paras-Govind <govindparas17@gmail.com>
2023-10-22 17:06:41 +0100

    Revamped main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import pygame_menu
 import pygame.freetype
+import pygame.event
 
 from common import *
 from text import Text, request_text
@@ -13,123 +14,155 @@ pygame.display.set_caption('Trick or Type')
 ghosts = pygame.sprite.Group()
 pumpkins = pygame.sprite.Group()
 
-code = ""
 
-def game_loop(code: str):
-    text = Text(gameDisplay, request_text())
-    print(code)
-    gameExit = False
+class Game:
+    def __init__(self) -> None:
+        self.gameDisplay = gameDisplay
+        self.code = ""
+        self.username = ""        
+        self.menu = pygame_menu.Menu(width=display_width, height=display_height,
+                        theme=pygame_menu.themes.THEME_DARK,
+                        title='Welcome')
+        self.create_room_menu = pygame_menu.Menu(width=display_width, height=display_height,
+                        theme=pygame_menu.themes.THEME_DARK,
+                        title='Room')
+        
+        self.username_box = self.menu.add.text_input(title="Username: ")
+        self.menu.add.button("Create room", self.create_room_loop)
 
-    ghostGap = 100
-    pumpGap = 50
-    darkness = 0
-    pumpFlag = True
-    
-    while not gameExit:
+        self.code_box = self.menu.add.text_input(title="Room code: ")
+        self.menu.add.button("Join room", self.join_room_loop)
+        self.menu.add.button("Quit", pygame_menu.events.EXIT)
+        self.menu.center_content()
+        
+    def message_display(self, text):
+        largeText = pygame.font.Font("assets/fonts/griffy.ttf", 100)
+        TextSurf, TextRect = text_objects(text, largeText)
+        TextRect.center = ((display_width/2),(display_height/2))
+        self.gameDisplay.blit(TextSurf, TextRect)
+        
+    def game_loop(self):
+        text = Text(self.gameDisplay, request_text())
+        gameExit = False
 
-        if darkness == 0:
+        ghostGap = 100
+        pumpGap = 50
+        darkness = 0
+        pumpFlag = True
+        
+        while not gameExit:
 
+            if darkness == 0:
+
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        return
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_ESCAPE:
+                            return
+                        if event.unicode == text.current_letter():
+                            text.next_letter()
+        
+                self.gameDisplay.fill(background_color)
+                text.text_surf.fill(background_color)
+                
+                i = 0
+                # render each letter of the current sentence one by one
+                for (idx, (letter, metric)) in enumerate(zip(text.current_text(), text.metrics)):
+                    if idx == text.letter_index:
+                        color = 'lightblue'
+                    elif idx < text.letter_index:
+                        color = 'red'
+                    else:
+                        color = 'lightgrey'
+                    Text.font.render_to(text.text_surf, (i, text.baseline), letter, color)
+                    i += metric[Text.M_ADV_X]
+                
+                self.gameDisplay.blit(text.text_surf, text.text_surf_rect)
+                pygame.display.flip()
+
+                finishedGhosts = []
+
+                for ghost in ghosts:
+                    if ghost.update(pygame.display.get_window_size()[0],pygame.display.get_window_size()[1],self.gameDisplay) == True:
+                        finishedGhosts.append(ghost)
+
+                for ghost in finishedGhosts:
+                    ghosts.remove(ghost)
+
+            else:
+
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        return
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+
+                        mousePos = pygame.mouse.get_pos()
+
+                        pumpkinsToRemove = []
+
+                        for pumpkin in pumpkins:
+                            if pumpkin.checkForInput(mousePos):
+                                darkness = darkness - 1
+                                pumpkinsToRemove.append(pumpkin)
+
+                        for pumpkin in pumpkinsToRemove:
+                            pumpkins.remove(pumpkin)
+
+                self.gameDisplay.fill((0, 0, 0))
+
+                for pumpkin in pumpkins:
+                    self.gameDisplay.blit(pumpkin.surf, pumpkin.rect)
+
+            pygame.display.update()
+            clock.tick(60)
+            ghostGap = ghostGap - 1
+            pumpGap = pumpGap - 1
+            if ghostGap == 0:
+                ghostGap = 100
+                newGhost = Ghost((0, 250), [False, True, True, False], 10, 0)
+                ghosts.add(newGhost)
+
+            if darkness == 0 and pumpFlag == True and pumpGap == 0:
+                pumpFlag = False
+
+                newPumpkin = Pumpkin((25, 250), 200)
+                pumpkins.add(newPumpkin)
+                darkness = darkness + 1
+                
+    def start_game(self):
+        # ping server
+        self.game_loop()
+    def create_room_loop(self):
+        self.code = "12345"
+        
+        # do something with code
+        
+        self.create_room_menu.add.label("Room code: " + self.code)
+        self.create_room_menu.add.button("Start", self.join_room_loop)
+        self.create_room_menu.add.button("Quit", pygame_menu.events.EXIT)
+        self.create_room_menu.mainloop(self.gameDisplay)
+
+    def join_room_loop(self):
+        self.code = self.code_box.get_value()
+        gameExit = False
+        while not gameExit:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
                 if event.type == pygame.KEYDOWN:
-                    if event.unicode == text.current_letter():
-                        text.next_letter()
-    
-            gameDisplay.fill(background_color)
-            text.text_surf.fill(background_color)
-            
-            i = 0
-            # render each letter of the current sentence one by one
-            for (idx, (letter, metric)) in enumerate(zip(text.current_text(), text.metrics)):
-                if idx == text.letter_index:
-                    color = 'lightblue'
-                elif idx < text.letter_index:
-                    color = 'red'
-                else:
-                    color = 'lightgrey'
-                Text.font.render_to(text.text_surf, (i, text.baseline), letter, color)
-                i += metric[Text.M_ADV_X]
-            
-            gameDisplay.blit(text.text_surf, text.text_surf_rect)
-            pygame.display.flip()
-
-            finishedGhosts = []
-
-            for ghost in ghosts:
-                if ghost.update(pygame.display.get_window_size()[0],pygame.display.get_window_size()[1],gameDisplay) == True:
-                    finishedGhosts.append(ghost)
-
-            for ghost in finishedGhosts:
-                ghosts.remove(ghost)
-
-        else:
-
+                    if event.key == pygame.K_ESCAPE:
+                        return
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    return
-                if event.type == pygame.MOUSEBUTTONDOWN:
+                # if event.type == server.EVENTTYPE: @ALEX
+                pass
+            self.gameDisplay.fill(white)
+            self.message_display(f"Room code: {self.code}. Waiting for host...")
+            pygame.display.update()
+            clock.tick(60)
 
-                    mousePos = pygame.mouse.get_pos()
-
-                    pumpkinsToRemove = []
-
-                    for pumpkin in pumpkins:
-                        if pumpkin.checkForInput(mousePos):
-                            darkness = darkness - 1
-                            pumpkinsToRemove.append(pumpkin)
-
-                    for pumpkin in pumpkinsToRemove:
-                        pumpkins.remove(pumpkin)
-
-            gameDisplay.fill((0, 0, 0))
-
-            for pumpkin in pumpkins:
-                gameDisplay.blit(pumpkin.surf, pumpkin.rect)
-
-        pygame.display.update()
-        clock.tick(60)
-        ghostGap = ghostGap - 1
-        pumpGap = pumpGap - 1
-        if ghostGap == 0:
-            ghostGap = 100
-            newGhost = Ghost((0, 250), [False, True, True, False], 10, 0)
-            ghosts.add(newGhost)
-
-        if darkness == 0 and pumpFlag == True and pumpGap == 0:
-            pumpFlag = False
-
-            newPumpkin = Pumpkin((25, 250), 200)
-            pumpkins.add(newPumpkin)
-            darkness = darkness + 1
-
-            
-menu = pygame_menu.Menu(width=display_width, height=display_height,
-                    theme=pygame_menu.themes.THEME_DARK,
-                    title='Welcome')
-
-
-create_room_menu = pygame_menu.Menu(width=display_width, height=display_height,
-                    theme=pygame_menu.themes.THEME_DARK,
-                    title='Room')
-
-create_room_menu.add.button("Start", game_loop, code)
-
-def create_room_loop():
-    code = "12345"
-    # do something with code
-    create_room_menu.mainloop(gameDisplay)
-
-username_box = menu.add.text_input(title="Username: ")
-menu.add.button("Create room", create_room_loop)
-
-code_box = menu.add.text_input(title="Room code: ")
-menu.add.button("Join room", game_loop)
-menu.add.button("Quit", pygame_menu.events.EXIT)
-menu.center_content()
-
-
-
-menu.mainloop(gameDisplay)
-pygame.quit()
-quit()
+if __name__ == "__main__":
+    game = Game()
+    game.menu.mainloop(game.gameDisplay)
+    pygame.quit()
+    quit()
