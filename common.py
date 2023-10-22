2f5d966ae9165412f2cce7fc44e3f65f51da0f91
AlexanderPezarro <alexanderpezarro@gmail.com>
2023-10-22 12:13:17 +0100

    Add config file to avoid pygame opening

--- common.py
+++ common.py
@@ -1,12 +1,5 @@
 import pygame
-
-
-display_width = 800
-display_height = 600
- 
-black = (0,0,0)
-white = (255,255,255)
-red = (255,0,0)
+from config import *
 
 clock = pygame.time.Clock()
 
---
+++ config.py
@@ -0,0 +1,8 @@
+display_width = 800
+display_height = 600
+ 
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+
+port=22435
